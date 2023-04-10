193b94d5e65b8671f6b9a3e466a6f2645ce56859
kimsehwan96 <sktpghks138@gmail.com>
2023-04-11 01:57:00 +0900

    Add nks resource provisioning terraform codes

--- terraform.tf
+++ terraform.tf
@@ -30,32 +30,83 @@ resource "ncloud_subnet" "private_subnet_1" {
     usage_type = "GEN"
 }
 
-resource "ncloud_subnet" "private_subnet_2" {
+resource "ncloud_subnet" "private_subnet_load_balancer_1" {
     vpc_no = ncloud_vpc.vpc.id
-    subnet = "10.0.2.0/24"
-    zone = "KR-2"
-    name = "private-subnet-2"
+    subnet = "10.0.200.0/24"
+    zone = "KR-1"
+    name = "private-subnet-lb-1"
     network_acl_no = ncloud_vpc.vpc.default_network_acl_no
     subnet_type = "PRIVATE"
-    usage_type = "GEN"
+    usage_type = "LOADB"
+}
+resource "ncloud_login_key" "loginkey" {
+  key_name = "numble-cluster-login-key"
 }
 
-resource "ncloud_subnet" "public_subnet_1" {
-    vpc_no = ncloud_vpc.vpc.id
-    subnet = "10.0.3.0/24"
-    zone = "KR-1"
-    name = "public-subnet-1"
-    network_acl_no = ncloud_vpc.vpc.default_network_acl_no
-    subnet_type = "PUBLIC"
-    usage_type = "GEN"
+data "ncloud_nks_versions" "version" {
+  filter {
+    name = "value"
+    values = ["1.24"]
+    regex = true
+  }
 }
 
-resource "ncloud_subnet" "public_subnet_2" {
-    vpc_no = ncloud_vpc.vpc.id
-    subnet = "10.0.4.0/24"
-    zone = "KR-2"
-    name = "public-subnet-2"
-    network_acl_no = ncloud_vpc.vpc.default_network_acl_no
-    subnet_type = "PUBLIC"
-    usage_type = "GEN"
+resource "ncloud_nks_cluster" "cluster" {
+  cluster_type           = "SVR.VNKS.STAND.C002.M008.NET.SSD.B050.G002"
+  k8s_version            = data.ncloud_nks_versions.version.versions.0.value
+  login_key_name         = ncloud_login_key.loginkey.key_name
+  name                   = "my-cluster"
+  lb_private_subnet_no   = ncloud_subnet.private_subnet_load_balancer_1.id
+  kube_network_plugin    = "cilium"
+  subnet_no_list         = [ ncloud_subnet.private_subnet_1.id ]
+  vpc_no                 = ncloud_vpc.vpc.id
+  zone                   = "KR-1"
+  log {
+    audit = true
+  }
+}
+
+data "ncloud_server_image" "image" {
+  filter {
+    name = "product_name"
+    values = ["ubuntu-20.04"]
+  }
+}
+
+data "ncloud_server_product" "product" {
+  server_image_product_code = data.ncloud_server_image.image.product_code
+
+  filter {
+    name = "product_type"
+    values = [ "STAND" ]
+  }
+
+  filter {
+    name = "cpu_count"
+    values = [ 2 ]
+  }
+
+  filter {
+    name = "memory_size"
+    values = [ "8GB" ]
+  }
+
+  filter {
+    name = "product_code"
+    values = [ "SSD" ]
+    regex = true
+  }
+}
+
+resource "ncloud_nks_node_pool" "node_pool" {
+  cluster_uuid   = ncloud_nks_cluster.cluster.uuid
+  node_pool_name = "numble-node-pool"
+  node_count     = 1
+  product_code   = data.ncloud_server_product.product.product_code
+  subnet_no      = ncloud_subnet.private_subnet_1.id
+  autoscale {
+    enabled = false
+    min = 1
+    max = 1
+  }
 }
